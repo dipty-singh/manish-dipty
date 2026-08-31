1f91cf5a6e02a807d1949ff95f07e02e5a33c0d9
Dipty Singh <dipty.singh14@gmail.com>
2026-08-31 22:30:49 +0530

    RSVP sheet updated

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 
 const CONFIG = {
     // ⭐ Your App Script URL
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzc1VyrPxeW6FCnBFaoIeubqFTPOvTPuoW3VswBlkZ9_X2AUlu2Q19QgBYomWpzvNhs/exec",
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbx4Mu2lapK3OLpv5Qz7EHEl0fshCSbHkvn3PqKZxmR3lxVWybNKJPJz3HB-6htpE1K5/exec",
     WEDDING_DATE: "2027-02-11T20:00:00"
 };
